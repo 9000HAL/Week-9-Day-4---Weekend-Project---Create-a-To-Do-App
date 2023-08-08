154d096fe2ce005e264e03f07f3f81bffcbc7537
9000HAL <gsanchezscs@gmail.com>
2023-08-08 11:49:20 -0700

    header adjust

--- to-do-list-app/src/components/Navbar.tsx
+++ to-do-list-app/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <span className="navbar-brand mb-5 h1">To Do List App</span>
+        <span className="navbar-brand mb-5 h1"><h1>Anti-ADHD Simple & Encouraging To Do List App</h1></span>
       </div>
     </nav>
   );
